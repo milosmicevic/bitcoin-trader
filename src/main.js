16e8038c9bb73bf83b48a259f9d4f729f0fdfab0
Milos Micevic <milos.micevic@asioso.com>
2023-01-12 18:16:44 +0100

    Some Adjustments

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import "./style.scss";
+
+const countDownTimer = document.querySelector(".countdown");
+const countDownDate = new Date(new Date().getTime() + 9.58 * 60000);
+
+const countDown = setInterval(() => {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  countDownTimer.innerHTML = `${minutes}:${seconds}`;
+
+  if (distance < 0) {
+    clearInterval(countDown);
+    countDownTimer.innerHTML = "EXPIRED";
+  }
+}, 1000);
